11d673821fa11cf83ce73ac6c8ba8dd67c0aef99
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-09-27 00:09:26 +0300

    HomeView mvp realization

---
+++ src/services/api/index.js
@@ -0,0 +1,1 @@
+export { default as getPopular } from './getPopular'
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import * as API from '../services/api'
 
 export default function HomeView() {
-  return <div>HomeView</div>
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    ;(async function () {
+      const { results: movies } = await API.getPopular()
+      if (movies) setMovies(movies)
+    })()
+  }, [])
+
+  return (
+    <section>
+      {movies && (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>{movie.original_title}</li>
+          ))}
+        </ul>
+      )}
+    </section>
+  )
 }
